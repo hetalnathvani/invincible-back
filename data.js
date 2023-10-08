2b49ca481f8814bbfc10e0749c6c9ef80e7293fc
Hetal Nathvani <nathvanihetal@gmail.com>
2023-10-08 22:20:08 +0530

    Initial Commit

---
+++ data.js
@@ -0,0 +1,17 @@
+const users = [
+  {
+    id: 1,
+    name: "Hetal Nathvani",
+    email: "hetal@openxcellinc.com",
+  },
+  {
+    id: 2,
+    name: "Jayani Nathvani",
+    email: "jayani@openxcellinc.com",
+  },
+  {
+    id: 1,
+    name: "Jayant Nathvani",
+    email: "jayant@openxcellinc.com",
+  },
+];
